ad3a27459b7ab18935aa690422b0eeea446c387b
Clément Horesh <49487620+clement-horesh@users.noreply.github.com>
2025-08-03 13:17:26 +0200

    Responsive hero img

--- src/components/hero-image.tsx
+++ src/components/hero-image.tsx
@@ -21,23 +21,23 @@ export function HeroImage() {
   }, []);
 
   return (
-    <div className="pt-25 w-full max-w-7xl h-150 overflow-hidden relative z-20">
+    <div className="pt-15 sm:pt-20 lg:pt-25 w-full max-w-7xl h-80 sm:h-120 lg:h-150 overflow-hidden relative z-20">
       <div className="relative max-w-[91%] mx-auto">
         {/* Background cards for stacked effect - vertical stacking with scaling */}
         <div 
-          className={`absolute -top-10 left-1/2 transform backdrop-blur-xs -translate-x-1/2 w-[90%] h-full bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 dark:border-gray-300/10 scale-100 transition-all duration-1000 ease-out ${
+          className={`absolute -top-2 sm:-top-4 lg:-top-10 left-1/2 transform backdrop-blur-xs -translate-x-1/2 w-[90%] h-full bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 dark:border-gray-300/10 scale-100 transition-all duration-1000 ease-out ${
             showCard1 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
           }`}
         ></div>
         <div 
-          className={`absolute -top-5 left-1/2 transform backdrop-blur-xs -translate-x-1/2 w-[95%] h-full bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 dark:border-gray-300/10 scale-100 transition-all duration-1000 ease-out ${
+          className={`absolute -top-1 sm:-top-2 lg:-top-5 left-1/2 transform backdrop-blur-xs -translate-x-1/2 w-[95%] h-full bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 dark:border-gray-300/10 scale-100 transition-all duration-1000 ease-out ${
             showCard2 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
           }`}
         ></div>
 
         {/* Main card */}
         <div 
-          className={`relative bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 backdrop-blur-xs dark:border-gray-300/10 p-5 transition-all duration-1000 ease-out ${
+          className={`relative bg-gray-300/50 dark:bg-gray-800/50 rounded-lg border border-gray-600/10 backdrop-blur-xs dark:border-gray-300/10 p-1 sm:p-2 lg:p-5 transition-all duration-1000 ease-out ${
             showMainCard ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
           }`}
         >
--- src/components/hero-text.tsx
+++ src/components/hero-text.tsx
@@ -6,7 +6,7 @@ export function HeroText() {
   return (
     <div className="flex flex-col gap-6 lg:gap-4 text-center sm:text-left relative z-20">
       <h1 className="text-5xl sm:text-5xl lg:text-6xl max-w-3xl font-bold bg-gradient-to-r from-black via-gray-700 to-gray-500 dark:from-gray-400 dark:via-gray-200 dark:to-white bg-clip-text text-transparent leading-none">
-      Levez la lumière <br/>sur vos données.
+      Levez la <span className="bg-gradient-to-r from-pink-300 via-orange-300 to-yellow-300 bg-clip-text text-transparent drop-shadow-[0_0_8px_rgba(251,146,60,0.3)] dark:drop-shadow-[0_0_10px_rgba(251,146,60,0.4)] animate-[pulse_2s_ease_forwards]">lumière</span> <br/>sur vos données.
 
       </h1>
       <p className="text-base sm:text-lg max-w-2xl text-muted-foreground leading-relaxed">
